fbea7e9adbeffaf779bb05a08cd25daf570f862a
dangilkerson <dangilkerson@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2009-01-21 06:38:00 +0000

    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@63 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

---
+++ Plugins/CodeRush Plugin/CR_ApprovalTests.Specs/OptionsSpec.cs
@@ -0,0 +1,29 @@
+﻿using ApprovalTests.Extensions.WinForms;
+using Machine.Specifications;
+using NUnit.Framework;
+
+namespace CR_ApprovalTests.Tests
+{
+	[TestFixture]
+	public class aaaa
+	{
+		[Test]
+		public void aaaaaa()
+		{
+			Assert.AreEqual("aaa", "aaa");
+
+			var options = new Options();
+			options.ShouldBeApproved();
+		}
+	}
+
+
+	[Subject(typeof(Options))]
+	public class when_displaying_defaults
+	{
+		Establish context =()=> options = new Options();
+        It should_display_them =()=> options.ShouldBeApproved();
+
+		static Options options;
+	}
+}
