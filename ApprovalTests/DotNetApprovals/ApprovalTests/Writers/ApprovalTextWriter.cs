2a1cff60216224aa1aeb87298dd1d4f534984a6e
dangilkerson <dangilkerson@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2009-06-16 21:27:15 +0000

    Added Binary Writer
    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@112 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

--- ApprovalTests/DotNetApprovals/ApprovalTests/Writers/ApprovalTextWriter.cs
+++ ApprovalTests/DotNetApprovals/ApprovalTests/Writers/ApprovalTextWriter.cs
@@ -1,33 +1,46 @@
 using System.IO;
 using ApprovalTests.Core;
+using System;
 
 namespace ApprovalTests
 {
 	public class ApprovalTextWriter : IApprovalWriter
 	{
-		private readonly string data;
-
 		public ApprovalTextWriter(string data)
 		{
-			this.data = data;
+			Data = data;
 		}
+
+		public ApprovalTextWriter(string data, string extensionWithoutDot)
+		{
+			Data = data;
+			ExtensionWithDot = EnsureDoc(extensionWithoutDot);
+		}
+
+		private string EnsureDoc(string extension)
+		{
+			string extensionWithDot = String.Format(".{0}", extension);
+			return extension.StartsWith(".") ? extension : extensionWithDot;
+		}
+        public string Data { get; set; }
+		public string ExtensionWithDot { get; set; }
 
 		#region IApprovalWriter Members
 
 		public string GetApprovalFilename(string basename)
 		{
-			return basename + ".approved.txt";
+			return String.Format("{0}.approved{1}", basename, ExtensionWithDot);
 		}
 
 		public string GetReceivedFilename(string basename)
 		{
-			return basename + ".received.txt";
+			return String.Format("{0}.received{1}", basename, ExtensionWithDot);
 		}
 
 		public string WriteReceivedFile(string received)
 		{
 			Directory.CreateDirectory(Path.GetDirectoryName(received));
-			File.WriteAllText(received, data);
+			File.WriteAllText(received, Data);
 			return received;
 		}
 
