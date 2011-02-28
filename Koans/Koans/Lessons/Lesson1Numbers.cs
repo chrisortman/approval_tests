89307f1769f9cd8cf3225c667b13f180a827ea8e
isidore_usa <isidore_usa@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2011-02-28 08:41:52 +0000

    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@222 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

---
+++ Koans/Koans/Lessons/Lesson1Numbers.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ____ = System.String;
+namespace TestProject1
+{
+    [TestClass]
+    public class Lesson1Numbers
+    {
+        /*
+         * How to Run: Press Ctrl+R,T (go ahead, try it now)
+         * Step 1: find the 1st method that fails
+         * Step 2: Fill in the blank ____ to make it pass
+         * Step 3: run it again
+         * Note: Do not change anything other than the blank
+         */
+        [TestMethod]
+        public void NumbersDontNeedQuotes()
+        {
+            Assert.AreEqual(___, 42);
+            
+        }
+        [TestMethod]
+        public void NumbersCanHaveDecimals()
+        {
+            Assert.AreEqual(___, 4.2);
+        }
+
+        [TestMethod]
+        public void IntegerTypes()
+        {
+            Assert.AreEqual(typeof(int), ___.GetType());
+        }
+        [TestMethod]
+        public void DoubleTypes()
+        {
+            Assert.AreEqual(typeof(double), ___.GetType());
+        }
+        [TestMethod]
+        public void WhichTypeIs8()
+        {
+            Assert.IsInstanceOfType(8, (typeof(____)));
+        }
+        [TestMethod]
+        public void IsThisTheSameType()
+        {
+            Assert.IsInstanceOfType(8.0, (typeof(____)));
+        }
+        [TestMethod]
+        public void IntegerHaveLimits()
+        {
+            Assert.AreEqual(___, int.MaxValue);
+        }
+        [TestMethod]
+        public void LongHaveBiggerLimits()
+        {
+            Assert.AreEqual(___, long.MaxValue);
+        }
+        [TestMethod]
+        public void NegativeNumbers()
+        {
+            
+            Assert.AreEqual(___, -9);
+        }
+        [TestMethod]
+        public void NumbersAdd()
+        {
+
+            Assert.AreEqual(___, 2 + 3);
+        }
+        [TestMethod]
+        public void NumbersSubtract()
+        {
+
+            Assert.AreEqual(___,  10 - 3);
+        }
+        [TestMethod]
+        public void NumbersMultiply()
+        {
+
+            Assert.AreEqual(___, 5*5);
+        }
+        [TestMethod]
+        public void NumbersDivide()
+        {
+
+            Assert.AreEqual(___, 10/2);
+        }
+        [TestMethod]
+        public void DoublesDivideNormally()
+        {
+
+            Assert.AreEqual(___, 10.0 / 4);
+        }
+        [TestMethod]
+        public void IntegersRoundDownWhenDivided()
+        {
+
+            Assert.AreEqual(___, 10 / 4);
+        }
+        [TestMethod]
+        public void ButYouCanAlsoGetTheRemainer ()
+        {
+
+            Assert.AreEqual(___, 7 % 4);
+        }
+        [TestMethod]
+        public void RemainerIsNotModulus()
+        {
+
+            Assert.AreEqual(___, -6 % 4);
+        }
+        #region Ignore
+        public object ___ = "Please Fill in the blank";
+        #endregion
+    }
+}
---
+++ Koans/Koans/Tests/Lesson1NumbersTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class Lesson1NumbersTest
+    {
+        [TestMethod]
+        public void TestTypeOfQuestions()
+        {
+            AssertTypeOf<Lesson1Numbers>(l => l.WhichTypeIs8(), typeof(int));
+            AssertTypeOf<Lesson1Numbers>(l => l.IsThisTheSameType(), typeof(double));
+           
+        }
+
+        private void AssertTypeOf<T>(Action<T> test, Type expected) where T : new()
+        {
+            var lesson = new T();
+            string s = "Didn't Fail";
+            try
+            {
+               test(lesson);
+
+            }
+            catch (Exception e)
+            {
+                s = e.Message;
+            }
+            var expectedMessage =
+                String.Format(
+                    "Assert.IsInstanceOfType failed.  Expected type:<System.String>. Actual type:<{0}>.",
+                    expected);
+            Assert.AreEqual(s,expectedMessage);
+
+
+        }
+        
+
+        [TestMethod]
+        public void TestAllQuestions()
+        {
+            Verify(l => l.NumbersDontNeedQuotes(),  42);
+            Verify(l => l.NumbersCanHaveDecimals(), 4.2);
+            Verify(l => l.IntegerTypes(), 42);
+            Verify(l => l.DoubleTypes(), 4.2);
+            Verify(l => l.IntegerHaveLimits(), 2147483647);
+            Verify(l => l.NegativeNumbers(), -9);
+            Verify(l => l.NumbersAdd(), 5);
+            Verify(l => l.NumbersSubtract(), 7);
+            Verify(l => l.NumbersMultiply(), 25);
+            Verify(l => l.NumbersDivide(), 5);
+            Verify(l => l.DoublesDivideNormally(), 2.5);
+            Verify(l => l.IntegersRoundDownWhenDivided(), 2);
+            Verify(l => l.ButYouCanAlsoGetTheRemainer(), 3);
+            Verify(l => l.RemainerIsNotModulus(), -2);
+        }
+
+
+        public void Verify(Action<Lesson1Numbers> test, object answer)
+        {
+            AssertLesson(test, l => l.___ = answer);
+        }
+
+        public void AssertLesson<T>(Action<T> test, Action<T> answer) where T : new()
+        {
+            var l =  new T();
+            var failed = false;
+            try
+            {
+                test(l);
+            }
+            catch (Exception)
+            {
+                failed = true;
+            }
+            Assert.IsTrue(failed);
+            answer(l);
+            test(l);
+
+        }
+
+    }
+}
