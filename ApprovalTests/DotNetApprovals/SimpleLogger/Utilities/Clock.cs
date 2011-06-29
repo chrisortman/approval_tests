fc566904f818a0904a07feab2b4a5ec5df238bf5
jasonkerney <jasonkerney@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2011-06-29 15:24:34 +0000

    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@267 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

---
+++ ApprovalTests/DotNetApprovals/SimpleLogger/Utilities/Clock.cs
@@ -0,0 +1,24 @@
+using System;
+using ApprovalUtilities.Persistence;
+
+namespace ApprovalUtilities.Utilities
+{
+	public class Clock : ILoader<DateTime>
+	{
+		public virtual DateTime Load()
+		{
+			return DateTime.Now;
+		}
+	}
+
+	public class MockTimer : Clock
+	{
+		private int ticks;
+
+		public override DateTime Load()
+		{
+			ticks += 10;
+			return new DateTime(2011, 5, 6, 10, 30, 0, ticks);
+		}
+	}
+}
