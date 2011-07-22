4fc12310f0092b042fc4b60b7c28744c2e8f784f
jasonkerney <jasonkerney@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2011-07-22 16:01:49 +0000

    Updated logging to allow for guaranteed different files for different days.
    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@276 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

--- ApprovalTests/DotNetApprovals/SimpleLogger/SimpleLogger/Writers/FileWriter.cs
+++ ApprovalTests/DotNetApprovals/SimpleLogger/SimpleLogger/Writers/FileWriter.cs
@@ -4,47 +4,73 @@ using System.IO;
 
 namespace ApprovalUtilities.SimpleLogger.Writers
 {
-    public class FileWriter : IAppendable
-    {
-        public FileWriter()
-        {
-            LogFile = Path.GetTempPath() + "Logger.txt";
-        }
+	public class FileWriter : IAppendable
+	{
+		public FileWriter()
+		{
+			LogFile = Path.GetTempPath() + "Logger.txt";
+		}
 
-        public void AppendLine(string text)
-        {
-            lock (this)
-            {
-                File.AppendAllText(LogFile, text + Environment.NewLine);
-            }
-        }
+		public void AppendLine(string text)
+		{
+			lock (this)
+			{
+				File.AppendAllText(LogFile, text + Environment.NewLine);
+			}
+		}
 
-        private string logFile;
+		private string logFilePath;
+		private Func<string> getLogFile;
+		public Func<string> GetLogFile
+		{
+			get { return getLogFile; }
+			set
+			{
+				getLogFile = value;
+				InitialWrite(value());
+			}
+		}
 
-        public string LogFile
-        {
-            get { return logFile; }
-            set
-            {
-                logFile = value;
-                EnsureDirectoryExists(new FileInfo(logFile).Directory);
-                var message = "Logging to:" + logFile;
-                Console.WriteLine(message);
-                Debug.WriteLine(message);
-            }
-        }
 
-        public static void EnsureDirectoryExists(DirectoryInfo directory)
-        {
-            directory.Refresh();
-            if (directory.Exists)
-            {
-                return;
-            }
+		public string LogFile
+		{
+			get
+			{
+				var logFile = GetLogFile();
+				InitialWrite(logFile);
+				return logFile;
+			}
+			set
+			{
+				GetLogFile = () => value;
+				InitialWrite(value);
+			}
+		}
 
-            EnsureDirectoryExists(directory.Parent);
+		private void InitialWrite(string value)
+		{
+			if (value.Equals(logFilePath))
+			{
+				return;
+			}
+			logFilePath = value;
+			EnsureDirectoryExists(new FileInfo(value).Directory);
+			var message = "Logging to:" + value;
+			Console.WriteLine(message);
+			Debug.WriteLine(message);
+		}
 
-            directory.Create();
-        }
-    }
+		public static void EnsureDirectoryExists(DirectoryInfo directory)
+		{
+			directory.Refresh();
+			if (directory.Exists)
+			{
+				return;
+			}
+
+			EnsureDirectoryExists(directory.Parent);
+
+			directory.Create();
+		}
+	}
 }
