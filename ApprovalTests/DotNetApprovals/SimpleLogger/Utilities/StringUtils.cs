4d4683ba983294a8c925a74c87f4d09bb5f3a48a
jasonkerney <jasonkerney@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2011-10-06 23:35:37 +0000

    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@296 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

--- ApprovalTests/DotNetApprovals/SimpleLogger/Utilities/StringUtils.cs
+++ ApprovalTests/DotNetApprovals/SimpleLogger/Utilities/StringUtils.cs
@@ -3,27 +3,27 @@ using System.Text;
 
 namespace ApprovalUtilities.Utilities
 {
-    public static class StringUtils
-    {
-        public static string ToReadableString(this IEnumerable list)
-        {
-            var sb = new StringBuilder();
-            sb.Append("[");
-            foreach (object l in list)
-            {
-                sb.Append(l + ", ");
-            }
-            if (sb.Length >0)
-            {
-            	sb.Remove(sb.Length - 2, 2);
-            }
-            sb.Append("]");
-            return sb.ToString();
-        }
-
-			public static string FormatWith(this string mask, params object[] parameters)
+	public static class StringUtils
+	{
+		public static string ToReadableString(this IEnumerable list)
+		{
+			var sb = new StringBuilder();
+			sb.Append("[");
+			foreach (object l in list)
 			{
-				return string.Format(mask, parameters);
+				sb.Append(l + ", ");
 			}
-    }
+			if (sb.Length > 0)
+			{
+				sb.Remove(sb.Length - 2, 2);
+			}
+			sb.Append("]");
+			return sb.ToString();
+		}
+
+		public static string FormatWith(this string mask, params object[] parameters)
+		{
+			return string.Format(mask, parameters);
+		}
+	}
 }
