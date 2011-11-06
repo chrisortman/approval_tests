8cd2bfd5cc73f9379b7448d91d362015935c64aa
isidore_usa <isidore_usa@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2011-11-06 23:17:15 +0000

    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@309 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

--- ApprovalTests/DotNetApprovals/ApprovalTests.Tests/EntityFramework/CompanyList.cs
+++ ApprovalTests/DotNetApprovals/ApprovalTests.Tests/EntityFramework/CompanyList.cs
@@ -1,44 +1,38 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data.Objects;
 using System.Linq;
 using System.Text;
 using ApprovalUtilities.Persistence;
 using ApprovalUtilities.Persistence.EntityFramework;
-using ApprovalUtilities.SimpleLogger;
 using ApprovalUtilities.Utilities;
 
 namespace ApprovalTests.Tests.EntityFramework
 {
-	public class CompanyList
-	{
-		public static string GetCompanyRoster(string name)
-		{
-			return GetCompanyRoster(GetCompanyByName(name));
-		}
+    public class CompanyList
+    {
+        public static string GetCompanyRoster(string name)
+        {
+            return GetCompanyRoster(GetCompanyByName(name));
+        }
 
-		public static string GetCompanyRoster(ILoader<IEnumerable<Company>> companyByName)
-		{
-			var companies = companyByName.Load();
-			StringBuilder b = new StringBuilder();
-			b.Append("<html><body>");
-			foreach (var company in companies)
-			{
-				b.Append("<li>{0}</li>".FormatWith(company.Name));
-			}
-			b.Append("</body></html>");
-			return b.ToString();
-		}
+        public static string GetCompanyRoster(ILoader<IEnumerable<Company>> companyByName)
+        {
+            IEnumerable<Company> companies = companyByName.Load();
+            var b = new StringBuilder();
+            b.Append("<html><body>");
+            foreach (Company company in companies)
+            {
+                b.Append("<li>{0}</li>".FormatWith(company.Name));
+            }
+            b.Append("</body></html>");
+            return b.ToString();
+        }
 
-		public static LamdaArrayLoader<Company, ModelContainer> GetCompanyByName(string name)
-		{
-			Logger.MarkerIn();
-			return Loaders.Create(()=> new ModelContainer(), 
-				(m) => (from c in m.Companies
-			        where c.Name.StartsWith(name)
-							select c).Take(9) );
-		}
-
-	}
+        public static LamdaArrayLoader<Company, ModelContainer> GetCompanyByName(string name)
+        {
+            return Loaders.Create(() => new ModelContainer(),
+                                  (m) => (from c in m.Companies
+                                          where c.Name.StartsWith(name)
+                                          select c).Take(9));
+        }
+    }
 }
