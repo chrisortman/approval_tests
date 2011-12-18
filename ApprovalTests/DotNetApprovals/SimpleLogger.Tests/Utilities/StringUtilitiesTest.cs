5f1367bf6097b70fd951b3b140fe320a04dd16f3
isidore_usa <isidore_usa@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2011-12-18 19:02:53 +0000

    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@329 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

---
+++ ApprovalTests/DotNetApprovals/SimpleLogger.Tests/Utilities/StringUtilitiesTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalUtilities.Utilities;
+
+namespace ApprovalUtilities.Tests.Utilities
+{
+	[TestClass]
+	public class StringUtilitiesTest
+	{
+		[TestMethod]
+		public void TestToReadableString()
+		{ 
+		Assert.AreEqual("[]",new int[0].ToReadableString());
+		Assert.AreEqual("[1, 2, 3]",new int[]{1,2,3}.ToReadableString());
+		}
+	}
+}
