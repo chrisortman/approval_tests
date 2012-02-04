f21da18d71395dca035f225c1118eec77592d7a9
isidore_usa <isidore_usa@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2012-02-04 17:10:49 +0000

    Added XmlApprovals
    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@354 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

--- ApprovalTests/DotNetApprovals/ApprovalTests/Xml/Approvals.cs
+++ ApprovalTests/DotNetApprovals/ApprovalTests/Xml/Approvals.cs
@@ -1,29 +1,51 @@
+using System;
 using ApprovalUtilities.Xml;
 
 namespace ApprovalTests.Xml
 {
+	[Obsolete("Use XmlApprovals instead")]
 	public class Approvals
 	{
 		public static void ApproveXml(string xml)
 		{
-			ApproveText(xml,"xml",true);
+			XmlApprovals.VerifyXml(xml);
 		}
-		
+
 		public static void ApproveText(string text, string fileExtensionWithoutDot, bool safely)
 		{
-			text = XmlUtils.FormatXml(text, safe: safely);
-			ApprovalTests.Approvals.Approve(new ApprovalTextWriter(text,fileExtensionWithoutDot));
+			XmlApprovals.VerifyText(text,fileExtensionWithoutDot,safely);
 		}
 
-		
 
 		/// <summary>
-		/// Throws exception if Xml is incorrectly formatted
+		/// 	Throws exception if Xml is incorrectly formatted
 		/// </summary>
 		public static void ApproveXmlStrict(string xml)
 		{
-			ApproveText(xml, "xml", false);
+			XmlApprovals.VerifyXml(xml);
+		}
+	}
 
+	public class XmlApprovals
+	{
+		public static void VerifyXml(string xml)
+		{
+			VerifyText(xml, "xml", true);
+		}
+
+		public static void VerifyText(string text, string fileExtensionWithoutDot, bool safely)
+		{
+			text = XmlUtils.FormatXml(text, safe: safely);
+			ApprovalTests.Approvals.Approve(new ApprovalTextWriter(text, fileExtensionWithoutDot));
+		}
+
+
+		/// <summary>
+		/// 	Throws exception if Xml is incorrectly formatted
+		/// </summary>
+		public static void VerifyXmlStrict(string xml)
+		{
+			VerifyText(xml, "xml", false);
 		}
 	}
 }
