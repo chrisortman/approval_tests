37b753e3ddbc12fcbd40a693f074a5807f35f0aa
isidore_usa <isidore_usa@f1b7a238-4fab-4267-8bd8-c8a7134c77f3>
2012-02-13 21:21:31 +0000

    git-svn-id: https://approvaltests.svn.sourceforge.net/svnroot/approvaltests/trunk/projects@359 f1b7a238-4fab-4267-8bd8-c8a7134c77f3

--- ApprovalTests/DotNetApprovals/ApprovalTests/Approvals.cs
+++ ApprovalTests/DotNetApprovals/ApprovalTests/Approvals.cs
@@ -223,5 +223,44 @@ namespace ApprovalTests
 		{
 			VerifyFile(file.FullName);
 		}
+		public static void VerifyWithAdditionalFeedbackOnFailure(object text, Action<string> executeOnFailure)
+		{
+			Verify(new ExecutableLambda("" + text, executeOnFailure));
+		}
+		public static void VerifyWithAdditionalFeedbackOnFailure(object text, Func<string, string> executeOnFailure)
+		{
+			Verify(new ExecutableLambda("" + text, executeOnFailure));
+		}
+	}
+
+	public class ExecutableLambda : IExecutableQuery
+	{
+		private readonly string text;
+		private readonly Func<string, string> executeOnFailure;
+		public ExecutableLambda(string text, Action<string> executeOnFailure)
+			: this(text, s => { executeOnFailure(s); return null; })
+		{
+
+		}
+		public ExecutableLambda(string text, Func<string, string> executeOnFailure)
+		{
+			this.text = text;
+			this.executeOnFailure = executeOnFailure;
+		}
+
+		public string GetQuery()
+		{
+			return text;
+		}
+
+		public string ExecuteQuery(string query)
+		{
+			if (!string.IsNullOrEmpty(query))
+			{
+				return executeOnFailure(query);
+			}
+
+			return null;
+		}
 	}
 }
--- ApprovalTests/DotNetApprovals/ApprovalTests/Reporters/ExecutableQueryFailure.cs
+++ ApprovalTests/DotNetApprovals/ApprovalTests/Reporters/ExecutableQueryFailure.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using ApprovalTests.Core;
 using ApprovalUtilities.Persistence;
@@ -20,21 +19,35 @@ namespace ApprovalTests.Reporters
 		public void Report(string approved, string received)
 		{
 			reporter.Report(approved, received);
-			var r = RunQueryAndGetPath(received);
-			var a = RunQueryAndGetPath(approved);
+
+			var aResult = ExecuteQuery(approved);
+			var rResult = ExecuteQuery(received);
+			if (string.IsNullOrEmpty(aResult.Result) && string.IsNullOrEmpty(rResult.Result))
+			{
+				return;
+			}
+
+			var r = RunQueryAndGetPath(received, rResult);
+			var a = RunQueryAndGetPath(approved, aResult);
 			reporter.Report(a, r);
 		}
 
-		private string RunQueryAndGetPath(string fileName)
+		private string RunQueryAndGetPath(string fileName, QueryResult qResult)
 		{
-			if (!File.Exists(fileName)) return fileName;
-
-			var newQuery = File.ReadAllText(fileName).Trim();
-			var newResult = query.ExecuteQuery(newQuery);
 			var newFileName = fileName + FILE_ADDITION;
 			var header = "\t\tDo NOT approve\r\n\t\tThis File will be Deleted\r\n\t\tit is for feedback purposes only\r\n";
-			File.WriteAllText(newFileName, string.Format("{0}query:\r\n\r\n{1}\r\n\r\nresult:\r\n\r\n{2}", header, newQuery, newResult));
+			File.WriteAllText(newFileName, string.Format("{0}query:\r\n\r\n{1}\r\n\r\nresult:\r\n\r\n{2}", header, qResult.Query, qResult.Result));
 			return newFileName;
+		}
+		private QueryResult ExecuteQuery(string fileName)
+		{
+			if (!File.Exists(fileName))
+			{
+				return new QueryResult();
+			}
+			var newQuery = File.ReadAllText(fileName).Trim();
+			var newResult = query.ExecuteQuery(newQuery);
+			return new QueryResult() { Query = newQuery, Result = newResult };
 		}
 
 		public void CleanUp(string approved, string received)
@@ -42,5 +55,10 @@ namespace ApprovalTests.Reporters
 			File.Delete(approved + FILE_ADDITION);
 			File.Delete(received + FILE_ADDITION);
 		}
+	}
+	public class QueryResult
+	{
+		public string Query { get; set; }
+		public string Result { get; set; }
 	}
 }
